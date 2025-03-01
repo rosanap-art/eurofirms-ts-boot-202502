ddb458d487cc3a4706bdc0af5bf73ce1db0a476b
Rosana Perez <rosanaperezrios@gmail.com>
2025-03-01 12:35:23 +0100

    add register-user endpoint in api and test shell script #7

--- staff/rosana-perez/playground/api/index.ts
+++ staff/rosana-perez/playground/api/index.ts
@@ -1,9 +1,26 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
+import { SystemError } from './errors'
+import logic from './logic'
 
 const api = express()
 
+const jsonBodyExpress = express.json()
+
 api.get('/', (req, res) => {
   res.send('Hello, API!')
+})
+
+api.post("/users", jsonBodyExpress, (req, res) => {
+  try {
+    const { name, email, username, password } = req.body
+
+    logic.registerUser(name, email, username, password)
+
+    res.status(201).send()
+
+  } catch (error) {
+    res.status(500).json({ error: SystemError.name, message: error.message })
+  }
 })
 
 api.listen(8080, () => console.log('API is up'))
---
+++ staff/rosana-perez/playground/api/test/register-user.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{"name": "Peter Pan", "email": "peter@pan.com", "username": "peterpan", "password": "123123123"}' http://localhost:8080/users -v
